80dc8b428a26240b0208b7942ae2e07de6b235c2
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-10-02 22:13:55 +0530

    Updates

---
+++ precache-manifest.aa8ace9fc57c2387b717fb097fc15c95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785e383327dc7d3fcb0256d5eafd66e6",
+    "url": "/officialwebsite/index.html"
+  },
+  {
+    "revision": "1eac9889f2738f056d25",
+    "url": "/officialwebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6e172b8ef476956db79",
+    "url": "/officialwebsite/static/js/2.8118fb35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/officialwebsite/static/js/2.8118fb35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eac9889f2738f056d25",
+    "url": "/officialwebsite/static/js/main.b3b59d7e.chunk.js"
+  },
+  {
+    "revision": "6002ead94346d651e8f0",
+    "url": "/officialwebsite/static/js/runtime-main.1e0e6cb2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/officialwebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
